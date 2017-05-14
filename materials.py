651da6248e11486362fe492d44d2a7fde7eec12e
Michiel Kleinnijenhuis <michiel.kleinnijenhuis@ndcn.ox.ac.uk>
2017-05-14 17:02:42 +0100

    Removed unused functions from materials module.

--- materials.py
+++ materials.py
@@ -34,8 +34,6 @@ import mathutils
 
 import bpy
 
-from . import utils as nb_ut
-
 
 # =========================================================================== #
 # material assignment
@@ -155,42 +153,6 @@ def set_materials(me, mat):
         me.materials.append(mat)
 
 
-def make_material(name="Material",
-                  diffuse=[1, 0, 0, 1], specular=[1, 1, 1, 0.5], alpha=1):
-    """Create a basic Blender Internal material."""
-
-    mat = bpy.data.materials.new(name)
-    mat.diffuse_color = diffuse[:3]
-    mat.diffuse_shader = "LAMBERT"
-    mat.diffuse_intensity = diffuse[3]
-    mat.specular_color = specular[:3]
-    mat.specular_shader = "COOKTORR"
-    mat.specular_intensity = specular[3]
-    mat.alpha = alpha
-    mat.use_transparency = True
-    mat.ambient = 1
-
-    return mat
-
-
-def make_material_vc_blender(name, fpath=""):
-    """Create a material for vertexcolour mapping.
-
-    # http://blenderartists.org/forum/showthread.php?247846-Context-problem
-    #     bpy.context.scene.game_settings.material_mode = "GLSL"
-    #     if bpy.context.scene.render.engine = "BLENDER_RENDER"
-    #         bpy.context.space_data.show_textured_solid = True
-    """
-
-    ca = [bpy.data.materials]
-    name = nb_ut.check_name(name, fpath, ca)
-    mat = bpy.data.materials.new(name)
-    mat.use_vertex_color_paint = True
-    mat.use_vertex_color_light = True
-
-    return mat
-
-
 def get_golden_angle_colour(i):
     """Return the golden angle colour from an integer number of steps."""
 
@@ -248,221 +210,6 @@ def BR2CR(mat):
 # ========================================================================== #
 
 
-def set_curve_weights(ob, name, label=None, scalars=None):
-    """"""
-
-    for spline, scalar in zip(ob.data.splines, scalars):
-        for point, val in zip(spline.points, scalar):
-            point.co[3] = val
-
-
-def create_vg_annot(ob, fpath, name=""):
-    """Import an annotation file to vertex groups.
-
-    TODO: decide what is the best approach:
-    reading gifti and converting to freesurfer format (current) or
-    have a seperate functions for handling .gii annotations
-    (this can be found in commit c3b6d66)
-    """
-
-    nb_ob = nb_ut.active_nb_object()[0]
-    ca = [nb_ob.labelgroups]  # TODO: all other labelgroups
-    groupname = nb_ut.check_name(name, fpath, ca)
-
-    props = {"name": groupname,
-             "filepath": fpath}
-    labelgroup = nb_ut.add_item(nb_ob, "labelgroups", props)
-
-    mat = make_cr_mat_surface_sg(labelgroup)
-    set_materials(ob.data, mat)
-
-    nodes = mat.node_tree.nodes
-    links = mat.node_tree.links
-    attr = nodes["Attribute"]
-    emit = nodes["Emission"]
-    diff = nodes["Diffuse BSDF"]
-    links.new(attr.outputs["Color"], diff.inputs["Color"])
-    links.new(attr.outputs["Color"], emit.inputs["Color"])
-    attr.attribute_name = groupname
-
-    if fpath.endswith('.border'):
-        iol = bpy.types.NB_OT_import_overlays
-        borderlist = iol.read_borders(fpath)
-        create_polygon_layer_int(ob, borderlist)
-        # (for each border?, will be expensive: do one for every file now)
-        # this already takes a long time...
-
-        new_mats = []
-        for i, border in enumerate(borderlist):
-
-            ca = [ob.data.polygon_layers_int,
-                  bpy.data.materials]
-            name = nb_ut.check_name(border['name'], "", ca)
-
-            value = i + 1
-            diffcol = list(border['rgb']) + [1.0]
-
-            mat = make_cr_mat_basic(name, diffcol, mix=0.05)
-            new_mats.append(mat)
-
-            props = {"name": name,
-                     "value": value,
-                     "colour": diffcol}
-            nb_ut.add_item(labelgroup, "labels", props)
-
-        pl = ob.data.polygon_layers_int["pl"]
-        set_materials_to_polygonlayers(ob, pl, new_mats)
-
-    else:
-        iol = bpy.types.NB_OT_import_overlays
-        labels, ctab, names = iol.read_surfannot(fpath)
-
-        new_vgs = []
-        new_mats = []
-        for i, labelname in enumerate(names):
-
-            ca = [ob.vertex_groups,
-                  bpy.data.materials]
-            name = nb_ut.check_name(labelname, "", ca)
-
-            label = np.where(labels == i)[0]
-            value = ctab[i, 4]
-            diffcol = ctab[i, 0:4]/255
-
-            vg = set_vertex_group(ob, name, label)
-            new_vgs.append(vg)
-
-            mat = make_cr_mat_basic(name, diffcol, mix=0.05)
-            new_mats.append(mat)
-
-            props = {"name": name,
-                     "value": int(value),
-                     "colour": diffcol}
-            nb_ut.add_item(labelgroup, "labels", props)
-
-        set_materials_to_vertexgroups(ob, new_vgs, new_mats)
-
-
-def create_border_curves(ob, fpath, name=""):
-    """Import an border file and create curves."""
-
-    iol = bpy.types.NB_OT_import_overlays
-    borderlist = iol.read_borders(fpath)
-
-    ca = [bpy.data.objects]
-    groupname = nb_ut.check_name(name, fpath, ca)
-    bordergroup_ob = bpy.data.objects.new(groupname, object_data=None)
-    bpy.context.scene.objects.link(bordergroup_ob)
-    bordergroup_ob.parent = ob
-    props = {"name": groupname,
-             "filepath": fpath}
-    nb_ob = nb_ut.active_nb_object()[0]  # FIXME
-    bordergroup = nb_ut.add_item(nb_ob, "bordergroups", props)
-
-    for i, border in enumerate(borderlist):
-
-        ca = [bpy.data.objects,
-              bpy.data.materials]
-        name = nb_ut.check_name(border['name'], "", ca)
-
-        diffcol = list(border['rgb']) + [1.0]
-        mat = make_cr_mat_basic(name, diffcol, mix=0.05)
-
-        bevel_depth = 0.5
-        bevel_resolution = 10
-        iterations = 10
-        factor = 0.5
-
-        props = {"name": name,
-                 "group": bordergroup.name,
-                 "colour": diffcol}
-        nb_ut.add_item(bordergroup, "borders", props)
-
-        curve = bpy.data.curves.new(name=name, type='CURVE')
-        curve.dimensions = '3D'
-        curveob = bpy.data.objects.new(name, curve)
-        bpy.context.scene.objects.link(curveob)
-        clist = [ob.data.vertices[vi].co[:3] for vi in border['verts'][:, 0]]
-        nb_ut.make_polyline(curve, clist)
-        curveob.data.fill_mode = 'FULL'
-        curveob.data.bevel_depth = bevel_depth
-        curveob.data.bevel_resolution = bevel_resolution
-        curveob.parent = bordergroup_ob
-        mod = curveob.modifiers.new("smooth", type='SMOOTH')
-        mod.iterations = iterations
-        mod.factor = factor
-        set_materials(curveob.data, mat)
-
-
-def create_polygon_layer_int(ob, borderlist):
-    """Creates a polygon layer and sets value to the borderindex."""
-
-    me = ob.data
-    pl = me.polygon_layers_int.new("pl")
-    loopsets = [set([vi for vi in poly.vertices]) for poly in me.polygons]
-    for bi, border in enumerate(borderlist):
-        pi = [loopsets.index(set(tri)) for tri in border['verts']]
-        for poly in me.polygons:
-            if poly.index in pi:
-                # note that this overwrites double entries
-                pl.data[poly.index].value = bi
-
-
-def create_vg_overlay(ob, fpath, name="", is_label=False, trans=1):
-    """Create label/scalar overlay from a labelfile.
-
-    Note that a (freesurfer) labelfile can contain scalars.
-    If scalars are available and is_label=False (loaded from scalars-menu),
-    a scalar-type overlay will be generated.
-    """
-
-    iol = bpy.types.NB_OT_import_overlays
-    label, scalars = iol.read_surflabel(fpath, is_label)
-
-    if scalars:
-
-        ca = [ob.vertex_groups,
-              ob.data.vertex_colors,
-              bpy.data.materials]
-        name = nb_ut.check_name(name, fpath, ca)
-
-        vgscalars, scalarrange = nb_ut.normalize_data(scalars)
-
-        vg = set_vertex_group(ob, name, label, scalars)
-
-        scalargroup = []
-        props = {"name": name,
-                 "filepath": fpath,
-                 "range": scalarrange}
-        nb_ov = nb_ut.add_item(scalargroup, "scalars", props)
-
-        map_to_vertexcolours(ob, nb_ov, [vg], is_label)
-
-    else:
-        nb_ob = nb_ut.active_nb_object()[0]
-        ca = [nb_ob.labelgroups]  # TODO: checkagainst all other labelgroups
-        groupname = nb_ut.check_name(name, fpath, ca)
-        props = {"name": name,
-                 "filepath": fpath}
-        labelgroup = nb_ut.add_item(nb_ob, "labelgroups", props)
-
-        ca = [ob.vertex_groups,
-              bpy.data.materials]
-        name = nb_ut.check_name(name, fpath, ca)
-        vg = set_vertex_group(ob, name, label, scalars)
-
-        values = [label.value for label in labelgroup.labels] or [0]
-        value = max(values) + 1
-        diffcol = [random.random() for _ in range(3)] + [trans]
-        mat = make_cr_mat_basic(name, diffcol, mix=0.05)
-        set_materials_to_vertexgroups(ob, [vg], [mat])
-
-        props = {"name": name,
-                 "value": value,
-                 "colour": diffcol}
-        nb_ov = nb_ut.add_item(labelgroup, "labels", props)
-
-
 def set_vertex_group(ob, name, label=None, scalars=None):
     """Create a vertex group.
 
@@ -493,34 +240,6 @@ def set_vertex_group(ob, name, label=None, scalars=None):
     return vg
 
 
-def get_vidxs_in_groups(ob, vgs=None):
-    """Return the vertex indices within vertex groups."""
-
-    if vgs is None:
-        vgs = ob.vertex_groups
-
-    group_lookup = {g.index: g.name for g in vgs}
-    vidxs = {name: [] for name in group_lookup.values()}
-    for v in ob.data.vertices:
-        for g in v.groups:
-            if g.group in list(group_lookup.keys()):
-                vidxs[group_lookup[g.group]].append(v.index)
-
-    return vidxs
-
-
-def select_vertices_in_vertexgroups(ob, vgs=None):
-    """Select the vertices in the vertexgroups."""
-
-    if vgs is None:
-        vgs = ob.vertex_groups
-
-    group_lookup = {g.index: g.name for g in vgs}
-    for v in ob.data.vertices:
-        for g in v.groups:
-            v.select = g.group in list(group_lookup.keys())
-
-
 # =========================================================================== #
 # vertex group materials
 # =========================================================================== #
@@ -548,7 +267,8 @@ def assign_materialslots_to_faces(ob, vgs=None, mat_idxs=[]):
     for poly in me.polygons:
         for vi in poly.vertices:
             allgroups = [g.group for g in me.vertices[vi].groups]
-            for vgs_idx, mat_idx in zip(reversed(vgs_idxs), reversed(mat_idxs)):
+            for vgs_idx, mat_idx in zip(reversed(vgs_idxs),
+                                        reversed(mat_idxs)):
                 if vgs_idx in allgroups:
                     poly.material_index = mat_idx
     me.update()
@@ -598,26 +318,7 @@ def assign_materialslots_to_faces_pls(ob, pl=None, mat_idxs=[]):
 # =========================================================================== #
 
 
-def map_to_vertexcolours(ob, nb_ov, vgs=None, is_label=False, colourtype=""):
-    """Write vertex group weights to a vertex colour attribute.
-
-    A colourbar is prepared for scalar overlays.
-    """
-
-    name = nb_ov.name
-    mat = make_cr_mat_surface_sg(nb_ov)
-    set_materials_to_vertexgroups(ob, vgs=None, mats=[mat])
-
-#     set_materials_to_vertexgroups(ob, vgs, [mat])
-#
-#     vcs = ob.data.vertex_colors
-#     vc = vcs.new(name=name)
-#     ob.data.vertex_colors.active = vc
-#     ob = assign_vc(ob, vc, vgs)
-
-    return mat
-
-def assign_vc(ob, vertexcolours, vgs, labelgroup=[], colour=[0, 0, 0]):
+def assign_vc(ob, vertexcolours, vgs, labelgroup=None, colour=[0, 0, 0]):
     """Assign RGB values to the vertex_colors attribute.
 
     TODO: find better ways to handle multiple assignments to vertexgroups
@@ -625,7 +326,7 @@ def assign_vc(ob, vertexcolours, vgs, labelgroup=[], colour=[0, 0, 0]):
 
     me = ob.data
 
-    if labelgroup:
+    if labelgroup is not None:
         vgs_idxs = set([g.index for g in vgs])
         C = []
         for v in me.vertices:
@@ -643,9 +344,9 @@ def assign_vc(ob, vertexcolours, vgs, labelgroup=[], colour=[0, 0, 0]):
         gindex = vgs[0].index  # FIXME: assuming single vertex group here
         W = np.array([v.groups[gindex].weight for v in me.vertices])
         m = W > 0.00313066844250063
-        W[m] = 1.055 * ( np.power(W[m], (1.0 / 2.4) )) - 0.055
+        W[m] = 1.055 * (np.power(W[m], (1.0 / 2.4))) - 0.055
         W[~m] = 12.92 * W[~m]
-        C = np.transpose(np.tile(W, [3,1]))
+        C = np.transpose(np.tile(W, [3, 1]))
 
     for poly in me.polygons:
         for idx, vi in zip(poly.loop_indices, poly.vertices):
@@ -655,46 +356,10 @@ def assign_vc(ob, vertexcolours, vgs, labelgroup=[], colour=[0, 0, 0]):
 
     return ob
 
-
-def sum_vertexweights(v, vgs, group_lookup):
-    """Sums the weights over all vertexgroups of a vertex.
-
-    FIXME: this sums to >1 and is therefore not useable as rgb.
-    """
-
-    w = 0
-    for g in v.groups:
-        if g.group in list(group_lookup.keys()):
-            w += g.weight
-
-    return w
-
-
-def sRGB_to_linear(C):
-    """Converts sRGB to linear color space."""
-
-    if(C <= 0.0404482362771082):
-        L = C / 12.92
-    else:
-        L = pow(((C + 0.055) / 1.055), 2.4)
-
-    return L
-
-
-def linear_to_sRGB(L):
-    """Converts linear to sRGB color space."""
-
-    if (L > 0.00313066844250063):
-        C = 1.055 * (pow(L, (1.0 / 2.4))) - 0.055
-    else:
-        C = 12.92 * L
-
-    return C
-
-
 # =========================================================================== #
 # voxelvolume texture mapping
 # =========================================================================== #
+
 
 def load_surface_textures(name, directory, nframes):
     """"""
@@ -841,40 +506,8 @@ def make_cr_mat_basic(name, diff_col, mix=0.04,
     return mat
 
 
-def make_nodegroup_rgba(name="RGBAGroup", colour=[1, 1, 1, 1]):
-    """Create a nodegroup encapsulating an RGB(A)."""
-
-    group = bpy.data.node_groups.new(name, "ShaderNodeTree")
-    group.outputs.new("NodeSocketShader", "Color")
-    group.outputs.new("NodeSocketShader", "Color")
-
-    nodes = group.nodes
-    links = group.links
-
-    nodes.clear()
-    prefix = ""
-
-    output_node = nodes.new("NodeGroupOutput")
-    output_node.location = (-200, 0)
-
-    rgb = nodes.new("ShaderNodeRGB")
-    rgb.label = "RGB"
-    rgb.name = prefix + "RGB"
-    rgb.outputs[0].default_value = colour
-    rgb.location = 0, 0
-
-    links.new(rgb.outputs["Color"], output_node.inputs[0])
-    links.new(rgb.outputs["Color"].default_value[3], output_node.inputs[1])
-
-    return group
-
-
 def make_material_emit_cycles(name, emission):
     """Create a Cycles emitter material for lighting."""
-
-    scn = bpy.context.scene
-#     if not scn.render.engine == "CYCLES":
-#         scn.render.engine = "CYCLES"
 
     mat = (bpy.data.materials.get(name) or
            bpy.data.materials.new(name))
@@ -899,43 +532,6 @@ def make_material_emit_cycles(name, emission):
     emit.location = 600, -100
 
     links.new(emit.outputs["Emission"], out.inputs["Surface"])
-
-    return mat
-
-
-def make_material_emit_internal(name, emission, is_addition=False):
-    """Create a Blender Internal emitter material for lighting."""
-
-    scn = bpy.context.scene
-#     if not scn.render.engine == "BLENDER_RENDER":
-#         scn.render.engine = "BLENDER_RENDER"
-
-    mat = (bpy.data.materials.get(name) or
-           bpy.data.materials.new(name))
-    mat.use_nodes = True
-
-    nodes = mat.node_tree.nodes
-    links = mat.node_tree.links
-
-    if not is_addition:
-        nodes.clear()
-    prefix = ""
-
-    out = nodes.new("ShaderNodeOutput")
-    out.label = "Output"
-    out.name = prefix + "Output"
-    out.location = 800, -500
-
-    mtrl = nodes.new("ShaderNodeMaterial")
-    mtrl.label = "Material"
-    mtrl.name = prefix + "Material"
-    mtrl.material = bpy.data.materials[mat.name]
-    mtrl.location = 600, -600
-
-    mat.diffuse_color = emission["colour"][:3]
-    mat.emit = emission["strength"]
-
-    links.new(mtrl.outputs["Color"], out.inputs["Color"])
 
     return mat
 
@@ -1182,10 +778,6 @@ def make_cr_matgroup_tract_sg(diffcol, mix=0.04, nb_ov=None):
     diffuse = {'colour': diffcol, 'roughness': 0.1}
     glossy = {'colour': (1.0, 1.0, 1.0, 1.0), 'roughness': 0.1}
 
-    scn = bpy.context.scene
-#     if not scn.render.engine == "CYCLES":
-#         scn.render.engine = "CYCLES"
-
     group = bpy.data.node_groups.new("TractOvGroup", "ShaderNodeTree")
     group.inputs.new("NodeSocketColor", "Color")
     group.outputs.new("NodeSocketShader", "Shader")
@@ -1260,11 +852,6 @@ def make_cr_matgroup_tract_sg(diffcol, mix=0.04, nb_ov=None):
 def make_material_bake_cycles(name, vcname=None, img=None):
     """Create a Cycles material to bake vc to texture."""
 
-    scn = bpy.context.scene
-    nb = scn.nb
-#     if not scn.render.engine == "CYCLES":
-#         scn.render.engine = "CYCLES"
-
     mat = (bpy.data.materials.get(name) or
            bpy.data.materials.new(name))
     mat.use_nodes = True
@@ -1303,80 +890,3 @@ def make_material_bake_cycles(name, vcname=None, img=None):
     links.new(attr.outputs["Color"], emit.inputs["Color"])
 
     return mat
-
-
-def make_nodegroup_mapframes(name="MapFramesGroup"):
-    """Create a nodegroup for mapping MR frames.
-
-    http://blender.stackexchange.com/questions/62110
-    """
-
-    group = bpy.data.node_groups.new(name, "ShaderNodeTree")
-    group.outputs.new("NodeSocketColor", "Vector")
-#
-#     nodes = group.nodes
-#     links = group.links
-#
-#     nodes.clear()
-#     prefix = ""
-#
-#     output_node = nodes.new("NodeGroupOutput")
-#     output_node.location = (-200, 0)
-#
-#     crgb = nodes.new("ShaderNodeCombineRGB")
-#     crgb.label = "Combine RGB"
-#     crgb.name = prefix + "Combine RGB"
-#     crgb.location = -400, 0
-#     crgb.hide = True
-#
-#     invt = nodes.new("ShaderNodeInvert")
-#     invt.label = "Invert"
-#     invt.name = prefix + "Invert"
-#     invt.location = -600, -150
-#     invt.hide = True
-#
-#     math1 = nodes.new("ShaderNodeMath")
-#     math1.label = "Add"
-#     math1.name = prefix + "MathAdd"
-#     math1.operation = 'ADD'
-#     math1.location = -800, -150
-#     math1.hide = True
-#
-#     math2 = nodes.new("ShaderNodeMath")
-#     math2.label = "Absolute"
-#     math2.name = prefix + "MathAbs2"
-#     math2.operation = 'ABSOLUTE'
-#     math2.location = -1000, -50
-#     math2.hide = True
-#
-#     math3 = nodes.new("ShaderNodeMath")
-#     math3.label = "Absolute"
-#     math3.name = prefix + "MathAbs1"
-#     math3.operation = 'ABSOLUTE'
-#     math3.location = -1000, 0
-#     math3.hide = True
-#
-#     srgb = nodes.new("ShaderNodeSeparateRGB")
-#     srgb.label = "Separate RGB"
-#     srgb.name = prefix + "Separate RGB"
-#     srgb.location = -1200, 0
-#     srgb.hide = True
-#
-#     tang = nodes.new("ShaderNodeTangent")
-#     tang.label = "Tangent"
-#     tang.name = prefix + "Tangent"
-#     tang.direction_type = 'UV_MAP'
-#     tang.location = -1400, 0
-#
-#     links.new(crgb.outputs["Image"], output_node.inputs[0])
-#     links.new(invt.outputs["Color"], crgb.inputs[2])
-#     links.new(math2.outputs["Value"], crgb.inputs[1])
-#     links.new(math3.outputs["Value"], crgb.inputs[0])
-#     links.new(math1.outputs["Value"], invt.inputs["Color"])
-#     links.new(math2.outputs["Value"], math1.inputs[1])
-#     links.new(math3.outputs["Value"], math1.inputs[0])
-#     links.new(srgb.outputs["G"], math2.inputs["Value"])
-#     links.new(srgb.outputs["R"], math3.inputs["Value"])
-#     links.new(tang.outputs["Tangent"], srgb.inputs["Image"])
-
-    return group
